0c10b08f70693697edd87e3fb07f247927de97ea
Sean Kozen <sgkozen@gmail.com>
2023-12-02 00:00:35 +0900

    google font manrope added

--- dist/index.64a4978e.js
+++ dist/index.64a4978e.js
@@ -577,10 +577,22 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 },{}],"goJYj":[function(require,module,exports) {
 const menuDiv = document.querySelector(".sidebar");
 const checkBox = document.getElementById("menu-checkbox");
-const translateMenu = ()=>{
+/***************************************/ /*****   Hamburger display or hide *****/ /***************************************/ const translateMenu = ()=>{
     checkBox.checked = false;
 };
 menuDiv.addEventListener("click", translateMenu);
+/***************************************/ /*****       Form Validation       *****/ /***************************************/ const senderName = document.getElementById("name");
+const nameValidation = document.getElementById("name_validation");
+const email = document.getElementById("email");
+const emailValidation = document.getElementById("email_validation");
+const message = document.getElementById("message");
+const messageValidation = document.getElementById("message_validation");
+console.log(senderName);
+console.log(nameValidation);
+console.log(email);
+console.log(emailValidation);
+console.log(message);
+console.log(messageValidation);
 
 },{}]},["ciGIJ","goJYj"], "goJYj", "parcelRequire2ee2")
 
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,29 @@
 const menuDiv = document.querySelector(".sidebar");
 const checkBox = document.getElementById("menu-checkbox");
 
+/***************************************/
+/*****   Hamburger display or hide *****/
+/***************************************/
+
 const translateMenu = () => {
   checkBox.checked = false;
 };
 
 menuDiv.addEventListener("click", translateMenu);
+
+/***************************************/
+/*****       Form Validation       *****/
+/***************************************/
+const senderName = document.getElementById("name");
+const nameValidation = document.getElementById("name_validation");
+const email = document.getElementById("email");
+const emailValidation = document.getElementById("email_validation");
+const message = document.getElementById("message");
+const messageValidation = document.getElementById("message_validation");
+
+console.log(senderName);
+console.log(nameValidation);
+console.log(email);
+console.log(emailValidation);
+console.log(message);
+console.log(messageValidation);
