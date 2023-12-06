40e1d668ec5c4a1dba838afc31f94c8a3571511e
Sean Kozen <sgkozen@gmail.com>
2023-12-07 00:45:21 +0900

    contact form wired up

--- dist/index.64a4978e.js
+++ dist/index.64a4978e.js
@@ -580,19 +580,7 @@ const checkBox = document.getElementById("menu-checkbox");
 /***************************************/ /*****   Hamburger display or hide *****/ /***************************************/ const translateMenu = ()=>{
     checkBox.checked = false;
 };
-menuDiv.addEventListener("click", translateMenu);
-/***************************************/ /*****       Form Validation       *****/ /***************************************/ const senderName = document.getElementById("name");
-const nameValidation = document.getElementById("name_validation");
-const email = document.getElementById("email");
-const emailValidation = document.getElementById("email_validation");
-const message = document.getElementById("message");
-const messageValidation = document.getElementById("message_validation");
-console.log(senderName);
-console.log(nameValidation);
-console.log(email);
-console.log(emailValidation);
-console.log(message);
-console.log(messageValidation);
+menuDiv.addEventListener("click", translateMenu); /***************************************/  /*****       Form Validation       *****/  /***************************************/ 
 
 },{}]},["ciGIJ","goJYj"], "goJYj", "parcelRequire2ee2")
 
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -14,16 +14,3 @@ menuDiv.addEventListener("click", translateMenu);
 /***************************************/
 /*****       Form Validation       *****/
 /***************************************/
-const senderName = document.getElementById("name");
-const nameValidation = document.getElementById("name_validation");
-const email = document.getElementById("email");
-const emailValidation = document.getElementById("email_validation");
-const message = document.getElementById("message");
-const messageValidation = document.getElementById("message_validation");
-
-console.log(senderName);
-console.log(nameValidation);
-console.log(email);
-console.log(emailValidation);
-console.log(message);
-console.log(messageValidation);
